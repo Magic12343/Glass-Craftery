080a9e7ba8a708a5876a0b86692765034e03d12d
Magic12343 <maxgamerspro@gmail.com>
2021-03-08 22:26:42 -0300

    Añadido Easter Egg
    ICKKCK

--- src/main/java/com/avalon/glasscraftery/GlassCraftery.java
+++ src/main/java/com/avalon/glasscraftery/GlassCraftery.java
@@ -7,20 +7,23 @@ import com.avalon.glasscraftery.core.init.BlockInit;
 import com.avalon.glasscraftery.core.init.ItemInit;
 import com.avalon.glasscraftery.world.OreGeneration;
 
+import net.minecraft.entity.item.PaintingType;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent.Register;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.IForgeRegistry;
 
 @Mod(GlassCraftery.MOD_ID)
-public class GlassCraftery {
+public class GlassCraftery 
+{
 	public static final Logger LOGGER = LogManager.getLogger();
 	public static final String MOD_ID = "glasscraftery";
-	public static final ItemGroup TUTORIAL_GROUP = new TutorialGroup("tutorialtab");
 
 	public GlassCraftery() {
 		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -36,16 +39,26 @@ public class GlassCraftery {
 	private void setup(final FMLCommonSetupEvent event) {
 
 	}
+	
+	{
 
-	public static class TutorialGroup extends ItemGroup {
+        FMLJavaModLoadingContext.get().getModEventBus().addGenericListener(PaintingType.class, this::registerPaintings);
+    }
 
-		public TutorialGroup(String label) {
-			super(label);
-		}
+		
+		 private void registerPaintings (Register<PaintingType> event) {
+		        
+		        final IForgeRegistry<PaintingType> registry = event.getRegistry();
+		        
+		        registry.register(this.createPainting("cabeson", 64, 32));
 
-		@Override
-		public ItemStack createIcon() {
-			return ItemInit.GLASS_CHAIN.get().getDefaultInstance();
-		}
+		    }
+		    
+		    private PaintingType createPainting (String id, int width, int height) {
+		        
+		        final PaintingType type = new PaintingType(width, height);
+		        type.setRegistryName("glasscraftery", id);
+		        return type;
+		    }
 	}
-}
+
